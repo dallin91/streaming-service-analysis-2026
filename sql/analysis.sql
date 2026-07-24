ffe8627b243e67bd056c2fd1d3f38e6681d0d141
Dallin Reeves <dallinreeves@gmail.com>
2026-07-24 13:42:53 -0500

    initial analysis looking at average rating by service and genre

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,2 +1,14 @@
 -- Run this file after running cleaning.sql
 
+-- First let's look at the average user/critic ratings broken down by service
+SELECT service, ROUND(AVG(user_rating), 2) AS avg_user_rating, ROUND(AVG(critic_score), 2) AS avg_critic_score
+FROM titles_with_pricing
+GROUP BY service
+ORDER BY AVG(user_rating) desc;
+
+-- Now let's drill down deeper by breaking it into genres
+SELECT service, title_genres.genre, COUNT(*) AS total_num_titles, ROUND(AVG(user_rating), 2) AS avg_user_rating, ROUND(AVG(critic_score), 2) AS avg_critic_score 
+FROM titles_with_pricing
+LEFT JOIN title_genres ON title_genres.id = titles_with_pricing.id
+GROUP BY service, genre
+ORDER BY AVG(user_rating) desc;
